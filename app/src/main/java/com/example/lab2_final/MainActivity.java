7d856fa3ef6beb7dbbd7e76706c3852d201690bc
kklimchukk <kirilklim4uk@gmail.com>
2023-12-18 23:55:54 +0200

    /

--- app/src/main/java/com/example/lab2_final/MainActivity.java
+++ app/src/main/java/com/example/lab2_final/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.lab2_final;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onButtonClick (View v) {
+        EditText el1 =(EditText) findViewById(R.id.Num1);
+        EditText el2 =(EditText) findViewById(R.id.Num2);
+        TextView resText = (TextView) findViewById(R.id.Result);
+
+        int num1 = Integer.parseInt(el1.getText().toString());
+        int num2 = Integer.parseInt(el2.getText().toString());
+        int resSum = num1 + num2;
+        resText.setText(Integer.toString(resSum));
+    }
 }
+
